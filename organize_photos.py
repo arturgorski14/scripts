72b66b2eba15388ebdda94ef8d7321057740debe
arturgorski14 <arturgorski14@gmail.com>
2023-09-22 22:25:03 +0200

    refactor: groupped functions by behaviour in classes

--- organize_photos.py
+++ organize_photos.py
@@ -1,64 +1,104 @@
+import argparse
 import re
 import os
 import shutil
 from collections import defaultdict
-from typing import Dict, List
+from typing import Dict, List, Pattern
 
-FOLDER_NAME_WITH_PHOTOS = r"C:\Users\Artur\Pictures\Screenshots\Z_telefonu"
-
-
-def main() -> None:
-    all_photo_files_by_date = get_photo_files_by_date(FOLDER_NAME_WITH_PHOTOS)
-    print(all_photo_files_by_date)
-    move_files_to_folders_by_date(FOLDER_NAME_WITH_PHOTOS, all_photo_files_by_date)
+CONFIG_FILE = f"organize_photos_config.ini"
 
 
-def get_photo_files_by_date(dirpath: str) -> Dict[str, List[str]]:
-    photo_files_by_date = defaultdict(list)
-    for filename in os.listdir(dirpath):  # filename is IMG_20220305_132952.jpg
-        fullpath = os.path.join(dirpath, filename)
+# TODO: Change language to English
+# TODO: Separate code
+# TODO: Clean main function
+# TODO: Write missing tests
 
-        # Check if the item is a file
-        if not os.path.isfile(fullpath):
-            continue
+class GetPhotosFromDirectory:
+    VALID_FILENAME_PATTERN: Pattern[str] = re.compile(r"^IMG_\d{8}_\d{6}\.jpg$")
 
-        # Extract the date from the filename (assuming the format is IMG_YYYYMMDD_...)
-        date_str = filename[4:12]
+    def __call__(self, dir_path) -> Dict[str, List[str]]:
+        photo_files_by_date = defaultdict(list)
+        for filename in os.listdir(dir_path):
+            if not self.validate_filename(filename):
+                continue
 
+            folder_name = self._prepare_destination_folder_name(filename)
+            photo_files_by_date[folder_name].append(filename)
+        return photo_files_by_date
+
+    def validate_filename(self, filename: str) -> bool:
+        # TODO: possible case of folder named with correct pattern
+        return bool(self.VALID_FILENAME_PATTERN.match(filename))
+
+    @staticmethod
+    def _prepare_destination_folder_name(name: str) -> str:
+        date_str = name[4:12]
         # Convert date to the desired format (YYYY.MM.DD)
         formatted_date = f"{date_str[:4]}.{date_str[4:6]}.{date_str[6:]}"
-
-        # Append the file's full path to the list for that date
-        photo_files_by_date[formatted_date].append(filename)
-    return photo_files_by_date
+        return formatted_date
 
 
-def move_files_to_folders_by_date(dirpath: str, files: Dict[str, List[str]]) -> None:
-    for date, files_by_date in files.items():
-        new_folder_path = os.path.join(dirpath, date)
+class MoveFilesToFolders:
+    def __call__(self, dir_path: str, data: Dict[str, List[str]]) -> None:
+        for folder_name, filenames in data.items():
+            new_folder_path = os.path.join(dir_path, folder_name)
 
-        # Use os.makedirs to create parent directories if they don't exist
-        os.makedirs(new_folder_path, exist_ok=True)
+            # Use os.makedirs to create parent directories if they don't exist
+            os.makedirs(new_folder_path, exist_ok=True)
 
-        for filename in files_by_date:
-            src_path = os.path.join(dirpath, filename)
-            dest_path = os.path.join(new_folder_path, filename)
+            for filename in filenames:
+                src_path = os.path.join(dir_path, filename)
+                destination_path = os.path.join(new_folder_path, filename)
+                self.move_file(src_path, destination_path)
 
-            # Use shutil.move for safer file moving (handles errors better)
-            try:
-                shutil.move(src_path, dest_path)
-                print(f"{src_path} -> {dest_path}")
-            except Exception as e:
-                print(f"Error moving '{src_path}' to '{dest_path}': {e}")
+    @staticmethod
+    def move_file(src: str, destination: str):
+        # Use shutil.move for safer file moving (handles errors better)
+        try:
+            shutil.move(src, destination)
+            print(f"{src} -> {destination}")
+        except Exception as e:
+            print(f"Error moving '{src}' to '{destination}': {e}")
 
 
-filename_pattern = re.compile(r'^IMG_\d{8}_\d{6}\.jpg$')
+def save_then_get_last_used_directory(dir_path) -> str:
+    if dir_path:
+        with open(CONFIG_FILE, "w") as conf:
+            conf.write(dir_path)
+    if os.path.exists(CONFIG_FILE):
+        with open(CONFIG_FILE, "r") as conf:
+            return conf.read()
 
 
-def is_valid_filename(filename):
-    # Use the precompiled pattern to check if the filename matches
-    return bool(filename_pattern.match(filename))
+def organize_photos() -> None:
+    parser = argparse.ArgumentParser(
+        prog="python organize_photos.py",
+        description="""
+        Zorganizuj swoje zdjęcia w folderach po datach.
+        Pierwsze użycie? Przekaż w parametrze directory_path pełną ścieżkę do katalogu ze zdjęciami.
+        Skrypt zapisuje ostatnio wykorzystany folder, przy kolejnym użyciu wykorzysta go :)
+        """,
+    )
+    parser.add_argument(
+        "-d",
+        "--directory_path",
+        default="",
+        required=False,
+    )
+    args = parser.parse_args()
+
+    source_directory_path = save_then_get_last_used_directory(args.directory_path)
+    if not source_directory_path:
+        parser.print_help()
+        return
+
+    get_data_command = GetPhotosFromDirectory()
+    all_photo_files_by_date = get_data_command(source_directory_path)
+    print(all_photo_files_by_date)
+    move_files_command = MoveFilesToFolders()
+    move_files_command(source_directory_path, all_photo_files_by_date)
+    print("Wszystkie pliki zostały przeniesione :)")
 
 
 if __name__ == "__main__":
-    main()
+    organize_photos()
--- test_organize_photos.py
+++ test_organize_photos.py
@@ -1,8 +1,15 @@
 import os
+import re
 import tempfile
+from typing import Pattern, Union
 
 import pytest
-from organize_photos import get_photo_files_by_date, move_files_to_folders_by_date
+from organize_photos import (
+    GetPhotosFromDirectory,
+    MoveFilesToFolders,
+)
+
+EXPECTED_FILENAME_PATTERN: Pattern[str] = re.compile(r"^IMG_\d{8}_\d{6}\.jpg$")
 
 
 # Define a fixture to create a temporary directory for testing
@@ -12,23 +19,69 @@ def temp_dir():
         yield temp_dir
 
 
-def given_folder_contains_files(dir_path, filenames):
+def given_folder_contains_files(dir_path, filenames: Union[list | str]):
+    if not isinstance(filenames, list):
+        filenames = [filenames]
     for filename in filenames:
         file_path = os.path.join(dir_path, filename)
         with open(file_path, "w") as f:
             f.write("Sample file content")
-    assert os.listdir(dir_path) == filenames, "ERROR: INCORRECTLY PREPARED DATA!"
+    assert sorted(os.listdir(dir_path)) == sorted(filenames), "ERROR: INCORRECTLY PREPARED DATA!"
+
+
+@pytest.mark.parametrize("filename", ["IMG_20220410_105512.jpg"])
+def test_is_valid_filename_success(filename):
+    command = GetPhotosFromDirectory()
+    assert (
+        command.validate_filename(filename) is True
+    ), f"{filename=} doesnt match pattern {EXPECTED_FILENAME_PATTERN}"
+
+
+@pytest.mark.parametrize(
+    "filename",
+    [
+        "not_matching_filenames.jpg",
+        "IM_20220410_105512.jpg",
+        "IMM_20220410_105512.jpg",
+        "IMMG_20220410_105512.jpg",
+        "IMG_202220410_105512.jpg",
+        "IMG_2020410_105512.jpg",
+        "IMG_20220410_1055512.jpg",
+        "IMG_20220410_10512.jpg",
+        "IMG_IMG_20220410_105512.jpg",
+        "IMG_20220410_105512.jpgg",
+        "IIMG_20220410_105512.jpg" "IMG_20220410_105512.jpgg",
+        "IMG_20220410_105512.png",
+        "IMG_20220410_105512.webp",
+        "IMG_20220410_105512.jpeg",
+        "IMG_20220410_105512.csv",
+    ],
+)
+def test_is_valid_filename_failure(filename):
+    command = GetPhotosFromDirectory()
+    assert command.validate_filename(filename) is False
 
 
 @pytest.mark.parametrize(
     "filenames, expected_result",
     [
         (
-          [],
-          {},
+            [
+                "folder",
+                "2022.04.10",
+                "not_matching_filename",
+                "IM_20220410_105512.jpg",
+                "IMM_20220410_105512.jpg",
+            ],
+            {},
         ),
         (
-            ["IMG_20220305_132952.jpg", "IMG_20220306_143021.jpg", "IMG_20220410_105512.jpg"],
+            [
+                "2022.03.05",
+                "IMG_20220305_132952.jpg",
+                "IMG_20220306_143021.jpg",
+                "IMG_20220410_105512.jpg",
+            ],
             {
                 "2022.03.05": ["IMG_20220305_132952.jpg"],
                 "2022.03.06": ["IMG_20220306_143021.jpg"],
@@ -46,9 +99,10 @@ def given_folder_contains_files(dir_path, filenames):
 def test_get_photo_files_by_date(temp_dir, filenames, expected_result):
     # Arrange
     given_folder_contains_files(temp_dir, filenames)
+    command = GetPhotosFromDirectory()
 
     # Act
-    result = get_photo_files_by_date(temp_dir)
+    result = command(temp_dir)
 
     # Assert
     assert len(result) == len(expected_result)
@@ -67,7 +121,11 @@ def test_get_photo_files_by_date(temp_dir, filenames, expected_result):
             {},
         ),
         (
-            ["IMG_20220305_132952.jpg", "IMG_20220306_143021.jpg", "IMG_20220410_105512.jpg"],
+            [
+                "IMG_20220305_132952.jpg",
+                "IMG_20220306_143021.jpg",
+                "IMG_20220410_105512.jpg",
+            ],
             {
                 "2022.03.05": ["IMG_20220305_132952.jpg"],
                 "2022.03.06": ["IMG_20220306_143021.jpg"],
@@ -87,7 +145,8 @@ def test_move_files_to_folders_by_date(temp_dir, filenames, expected_structure):
     given_folder_contains_files(temp_dir, filenames)
 
     # Act
-    move_files_to_folders_by_date(temp_dir, expected_structure)
+    command = MoveFilesToFolders()
+    command(temp_dir, expected_structure)
 
     # Assert
     # Check that files doesn't exist in parent folder
